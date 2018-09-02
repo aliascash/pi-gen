4f857dc6f6fa410e8c6c31c5328f732922f15150
HLXEasy <hlxeasy@gmail.com>
2018-09-02 18:58:50 +0200

    Implemented creation of parent directories to fix permission issue during first start of Spectrecoin daemon

--- stage3/01-install-spectrecoind/00-run.sh
+++ stage3/01-install-spectrecoind/00-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 SPECTRECOIN_VERSION=v2.0.6
 
+# ============================================================================
 # Install Spectrecoin binaries
 wget https://github.com/spectrecoin/spectre/releases/download/${SPECTRECOIN_VERSION}/Spectrecoin-${SPECTRECOIN_VERSION}-RaspberryPi.tgz
 tar xzf Spectrecoin-${SPECTRECOIN_VERSION}-RaspberryPi.tgz
@@ -11,9 +12,11 @@ install -v -o 1000 -g 1000 -m 744 usr/local/bin/spectrecoind    "${ROOTFS_DIR}/u
 rm -f /tmp/Spectrecoin-${SPECTRECOIN_VERSION}-RaspberryPi.tgz
 rm -rf usr/
 
+# ============================================================================
 # Bootstrap blockchain
 git clone https://github.com/spectrecoin/spectrecoin-blockchain-bootstrap.git
 
+install -d -o 1000 -g 1000 -m 755 "${ROOTFS_DIR}/home/pi/.spectrecoin/"
 install -d -o 1000 -g 1000 -m 755 "${ROOTFS_DIR}/home/pi/.spectrecoin/txleveldb/"
 
 install -v -o 1000 -g 1000 -m 600 spectrecoin-blockchain-bootstrap/txleveldb/*  "${ROOTFS_DIR}/home/pi/.spectrecoin/txleveldb/"
@@ -21,9 +24,11 @@ install -v -o 1000 -g 1000 -m 600 spectrecoin-blockchain-bootstrap/blk0001.dat  
 
 rm -rf spectrecoin-blockchain-bootstrap
 
+# ============================================================================
 # Install Spectrecoin-RPC-UI
 git clone https://github.com/HLXEasy/spectre-rpc-sh-ui.git
 
+install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/spectrecoin-rpc-sh-ui"
 install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/spectrecoin-rpc-sh-ui/include"
 install -d -o 1000 -g 1000 "${ROOTFS_DIR}/home/pi/spectrecoin-rpc-sh-ui/sample_config_daemon"
 install -v -o 1000 -g 1000 -m 644 spectre-rpc-sh-ui/include/*               "${ROOTFS_DIR}/home/pi/spectrecoin-rpc-sh-ui/include/"
@@ -34,6 +39,5 @@ install -v -o 1000 -g 1000 -m 755 spectre-rpc-sh-ui/spectre_rpc_ui.sh       "${R
 
 # Use config from RPC-UI also on wallet
 install -v -o 1000 -g 1000 -m 644 spectre-rpc-sh-ui/sample_config_daemon/spectrecoin.conf  "${ROOTFS_DIR}/home/pi/.spectrecoin/"
-
 
 rm -rf spectre-rpc-sh-ui/
