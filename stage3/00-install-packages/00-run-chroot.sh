84492a47dcabdb6280405c1485be07f5270f7e45
HLXEasy <hlxeasy@gmail.com>
2019-01-06 00:02:44 +0100

    Fixed build error because of missing option --allow-unauthenticated

--- stage3/00-install-packages/00-run-chroot.sh
+++ stage3/00-install-packages/00-run-chroot.sh
@@ -2,7 +2,7 @@
 echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list
 
 apt-get update -y
-apt-get install -y --no-install-recommends \
+apt-get install -y --no-install-recommends --allow-unauthenticated \
     libboost-chrono1.67.0 \
     libboost-filesystem1.67.0 \
     libboost-program-options1.67.0 \
