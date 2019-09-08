fd9839614a759bf4a1f8a459b7b9925696512a3e
HLXEasy <hlxeasy@gmail.com>
2019-09-08 21:40:01 +0200

    Backports repo no longer required

--- stage3/00-install-packages/00-run-chroot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-echo "deb http://ftp.debian.org/debian stretch-backports main" > /etc/apt/sources.list.d/stretch-backports.list
-
-apt-get update -y
-apt-get install -y --no-install-recommends --allow-unauthenticated \
-    libboost-chrono1.67.0 \
-    libboost-filesystem1.67.0 \
-    libboost-program-options1.67.0 \
-    libboost-thread1.67.0
