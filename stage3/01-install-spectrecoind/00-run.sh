701c5ff2035c3f1696124c143b1294f7435a9d9f
HLXEasy <hlxeasy@gmail.com>
2019-09-17 21:48:26 +0200

    #214: Fixed Tor key setup, see https://github.com/Stadicus/RaspiBolt/issues/343

--- stage3/01-install-spectrecoind/00-run.sh
+++ stage3/01-install-spectrecoind/00-run.sh
@@ -120,7 +120,7 @@ echo "${torRepo}" > tor_repo
 install -v -o 0 -g 0 -m 644 tor_repo "${ROOTFS_DIR}/etc/apt/sources.list.d/tor.list"
 rm -f tor_repo
 on_chroot << EOF
-gpg --keyserver keys.gnupg.net --recv 886DDD89
+curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 apt-get update
 apt-get install -y tor deb.torproject.org-keyring
