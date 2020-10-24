c223719369a0b81643953fa1df7ee2dbb99a5c79
HLXEasy <hlxeasy@gmail.com>
2020-10-24 18:29:28 +0200

    Implemented bootstrap installer service which downloads and installs the bootstrap archive during initial startup

---
+++ stage3/01-install-aliaswalletd/files/aliasbootstrapinstaller.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+# ============================================================================
+# Automatic installation of Alias blockchain bootstrap
+#
+# Created: 2020-10-24 HLXEasy
+# ============================================================================
+
+aliasDatafolder=${HOME}/.aliaswallet
+blkdataFile=${aliasDatafolder}/blk0001.dat
+txdateFolder=${aliasDatafolder}/txleveldb
+maxChainAge=3
+performBootstrapDownload=false
+bootstrapDownloadUrl=https://download.alias.cash/files/bootstrap/BootstrapChain.zip
+bootstrapArchive=${HOME}/BootstrapChain.zip
+
+# Check if blk0001.dat exists and if yes, determine if last modification time
+# is longer than ${maxChainAge} days ago
+if [ -e "${blkdataFile}" ] ; then
+    maxLastChainAccess=$(date -d "now - ${maxChainAge} days" +%s)
+    blkdataFileTime=$(date -r "${blkdataFile}" +%s)
+
+    if (( blkdataFileTime <= maxLastChainAccess )); then
+        echo "${blkdataFile} is older than ${maxChainAge} days"
+        performBootstrapDownload=true
+    fi
+else
+    performBootstrapDownload=true
+fi
+
+if ${performBootstrapDownload} ; then
+    if [ -e "${bootstrapArchive}" ] ; then
+        rm -rf "${bootstrapArchive}"
+    fi
+
+    # Shell-UI will check for this file to prevent service start in case
+    # the bootstrap chain download and install process is running
+    touch /run/bootstrapInstallerRunning
+
+    if wget -O "${bootstrapArchive}" ${bootstrapDownloadUrl} ; then
+        if [ -d "${aliasDatafolder}" ] ; then
+            if [ -e "${blkdataFile}" ] ; then
+                rm -f "${blkdataFile}"
+            fi
+            if [ -e "${txdateFolder}" ] ; then
+                rm -rf "${txdateFolder}"
+            fi
+        else
+            mkdir -p "${aliasDatafolder}"
+        fi
+        cd "${aliasDatafolder}" || exit
+        unzip "${bootstrapArchive}"
+    else
+        echo "Unable to download ${bootstrapDownloadUrl}"
+    fi
+
+    # Shell-UI can be used now
+    rm -f /run/bootstrapInstallerRunning
+fi
+
+sudo systemctl disable aliasbootstrapinstaller
+sudo systemctl enable aliaswalletd
+sudo systemctl start aliaswalletd
